37e51b8948980f4dc07073c0491b15ed75ad8142
Iram <iram1786@hotmail.com>
2018-07-17 11:40:55 +0100

    js

--- Tech day/map.js
+++ Tech day/map.js
@@ -14,12 +14,31 @@ if "Location" = var one
 };*/
 
 
-var location = 
-[(1, "Lars Magnus"), (1, "Reykjavik"), (1, "Nuuk"), (3, "Paris"), 
-(4, "Demo Room"), (4, "Kuala Lumpur"), (5, "Stockholm"), (5, "Berlin"), (5, "Helsinki"), (5, "Rome"),
-(6, "Wellington"), (6, "Canberra"), (6, "Tokyo"), (6, "Warsaw"), (6, "Kiev"), (6, "Moscow")
-(7, "Cape Town"), (7, "Jakarta")];
 
-for (key in location){
-    console.log(location[key])
+var txt
+$.getJSON("talks.json", function(json) {
+var obj = json;
+/* List of events*/
+var ZoneOneEvents =[];
+
+
+
+var myArray = [("Lars Magnus", "1"), ("Reykjavik", "1"), ("Nuuk", "1"), ("Paris", "3"), 
+("Demo Room", "4"), ("Kuala Lumpur", "4"), ("Stockholm", "5"), ("Berlin", "5"), ("Helsinki", "5"), ("Rome", "5"),
+("Wellington", "6"), ("Canberra", "6"), ("Tokyo", "6"), ("Warsaw", "6"), ("Kiev", "6"), ("Moscow", "6"),
+("Cape Town", "7"), ("Jakarta", "7")];
+/*
+for (var key in myArray){
+    console.log(myArray[key]);
+}*/
+
+for (var key in myArray)
+{ 
+    if key === obj[y].location {
+        zone = Array[Key]
+    }
+    if zone = 1{
+        push.ZoneOneEvents;
+    }
 }
+
