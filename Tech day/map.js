a8d435809f9cd7a377a0187ab86e9c28b5f0a34c
Iram <iram1786@hotmail.com>
2018-07-17 09:27:24 +0100

    map js added

---
+++ Tech day/map.js
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" media="screen" href="talks.json" />
+
+
+var one = ["Lars Magnus",  "Reykjavik", "Nuuk"];
+var three = ["Paris"];
+var four = ["Demo Room", "Kuala Lumpur"];
+var five = ["Stockholm", "Berlin", "Helsinki", "Rome"];
+var six = ["Wellington", "Canberra", "Tokyo", "Warsaw", "Kiev", "Moscow"];
+var seven = ["Cape Town", "Jakarta"];
+
+if "Location" = var one
+{ 
+    zone =1
+};
+
+
