2c3dc05c671ffc375430dedd3dff7f35ab4a5272
emmetconnolly1 <emmet.connolly@ericsson.com>
2018-07-18 10:44:21 +0100

    Map zones

--- Tech day/map.js
+++ Tech day/map.js
@@ -81,22 +81,20 @@ document.getElementById("zone1").innerHTML = z1txt;
 }
 
 
-/*zone 2*/
-/*
-var z2txt = '<table><tr><td>"Title"</td><td>"Time"</td></tr>';
-for(x=0; x<z1.length; x++){
+/*zone 1*/
+var z6txt = '<table><tr><td>"Title"</td><td>"Time"</td></tr>';
+for(x=0; x<z6.length; x++){
 
-    var aTitle = z1[x].Title; 
-    var aTime = z1[x].Time;  
+    var aTitle = z6[x].Title; 
+    var aTime = z6[x].Time;  
     
-    z2txt +='<tr><td>"'+ aTitle + '"</td><td>"' + aTime +'"</td></tr>';  
+    z6txt +='<tr><td>"'+ aTitle + '"</td><td>"' + aTime +'"</td></tr>';  
 }
-z2txt += "</table>";
+z6txt += "</table>";
 
 window.onload = function(){
-document.getElementById("zone2").innerHTML = z2txt;
+document.getElementById("zone6").innerHTML = z6txt;
 }
-*/
 
 
 
