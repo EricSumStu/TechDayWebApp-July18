f57b6d583d85b0740067521dd9933ada94013171
broao <aoife.broderick@ericsson.com>
2018-07-26 10:09:57 +0100

    reg page

--- Tech day/login.js
+++ Tech day/login.js
@@ -1,25 +1,23 @@
+
 //var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
-// var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-// if ( username == "admin" && password == "password"){
-    if (password == "password"){
-// alert ("Login successful. Welcome admin!");
-alert ("Welcome to Tech Day!");
-window.close("login.html");
-window.open("home.html"); // Redirecting to other page.
-return false;
-}
-else{
-//attempt --;// Decrementing by one.
-// alert("ACCESS DENIED. You have entered an incorrect username &/ password.");
-alert("ACCESS DENIED. You have entered an incorrect passcode.");
-// Disabling fields after 3 attempts.
-//if( attempt == 0){
-//document.getElementById("username").disabled = true;
-//document.getElementById("password").disabled = true;
-//document.getElementById("submit").disabled = true;
-return false;
-}
-}
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "admin" && password == "password"){
+    alert ("Login successful. Welcome admin!");
+    window.close("index.html");
+    window.open("admin.html"); // Redirecting to other page.
+    return false;
+    }
+    else{
+    //attempt --;// Decrementing by one.
+    alert("ACCESS DENIED. You have entered an incorrect username &/ password.");
+    // Disabling fields after 3 attempts.
+    //if( attempt == 0){
+    //document.getElementById("username").disabled = true;
+    //document.getElementById("password").disabled = true;
+    //document.getElementById("submit").disabled = true;
+    return false;
+    }
+    }
